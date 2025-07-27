f099b94b0b39bc25cf954f16c0f6247e2a2da023
Raihan-Karim <raihankarim1234@gmail.com>
2025-07-27 10:50:26 +0600

    new task

---
+++ p6.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <body>
+    <script>
+        var name = "Hamim";
+    var age = 25;
+    var sec = "A";
+    var mark = 80;
+    var isStudent = true;
+    document.write("Name:"+ name + "<br>");
+    
+    document.write("Age:", age , "<br>");
+    document.write("Section:" , sec, "<br>");
+    document.write("Mark:" , mark, "<br>");
+
+    //document.write("\n OPERATORS");
+    var a = 10;
+    var b = 3;
+    document.write("Addition:", a + b, "<br>");
+    document.write("Subtraction:", a - b, "<br>");
+    document.write("Multiplication:", a * b, "<br>");
+    document.write("Division:", a / b, "<br>");
+    document.write("Remainder:", a % b, "<br>");
+    //document.write("Is Student?", isStudent);
+
+    if(mark>=50){
+        document.write("Hamim is Passed","<br>");
+    }
+    </script>
+    </body>
+</head>
